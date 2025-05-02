9482392250a3cf72db32bb67e518113e44eb7f72
Hazim <hazim.shaifulbahari@cimb.com>
2025-05-02 13:08:39 +0800

    setup routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExchangeRatesComponent } from './components/exchange-rates/exchange-rates.component';
+import { HistoricalTrendsComponent } from './components/historical-trends/historical-trends.component';
+import { CurrencyConverterComponent } from './components/currency-converter/currency-converter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'exchange-rates', pathMatch: 'full' },
+  { path: 'exchange-rates', component: ExchangeRatesComponent },
+  { path: 'historical-trends', component: HistoricalTrendsComponent },
+  { path: 'currency-converter', component: CurrencyConverterComponent },
+  { path: '**', redirectTo: 'exchange-rates' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
