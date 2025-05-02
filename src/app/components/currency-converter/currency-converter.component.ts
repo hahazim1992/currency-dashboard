16c245610dabfdc0d1aa19647d2f5df9bc92ed36
Hazim <hazim.shaifulbahari@cimb.com>
2025-05-02 13:03:23 +0800

    currency converter FE implementation

--- src/app/components/currency-converter/currency-converter.component.ts
+++ src/app/components/currency-converter/currency-converter.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { ExchangeRateService } from 'src/app/services/exchange-rate.service';
 
 @Component({
   selector: 'app-currency-converter',
   templateUrl: './currency-converter.component.html',
-  styleUrls: ['./currency-converter.component.scss']
+  styleUrls: ['./currency-converter.component.scss'],
 })
 export class CurrencyConverterComponent {
+  amount: number = 1;
+  fromCurrency: string = 'USD';
+  toCurrency: string = 'EUR';
+  convertedAmount: number | null = null;
+  exchangeRates: any = {};
 
+  constructor(private exchangeRateService: ExchangeRateService) {}
+
+  ngOnInit() {
+    this.exchangeRateService.getExchangeRates().subscribe((data) => {
+      this.exchangeRates = data.conversion_rates;
+    });
+  }
+
+  convert() {
+    if (this.exchangeRates[this.fromCurrency] && this.exchangeRates[this.toCurrency]) {
+      const rate = this.exchangeRates[this.toCurrency] / this.exchangeRates[this.fromCurrency];
+      this.convertedAmount = this.amount * rate;
+    }
+  }
 }
