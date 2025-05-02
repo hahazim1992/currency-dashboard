22fc993d80bbd02a503f27a551207b758a67759a
Hazim <hazim.shaifulbahari@cimb.com>
2025-05-02 12:59:01 +0800

    exchange rates implementation

--- src/app/components/exchange-rates/exchange-rates.component.ts
+++ src/app/components/exchange-rates/exchange-rates.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ExchangeRateService } from 'src/app/services/exchange-rate.service';
 
 @Component({
   selector: 'app-exchange-rates',
   templateUrl: './exchange-rates.component.html',
-  styleUrls: ['./exchange-rates.component.scss']
+  styleUrls: ['./exchange-rates.component.scss'],
 })
-export class ExchangeRatesComponent {
+export class ExchangeRatesComponent implements OnInit {
+  exchangeRates: any[] = [];
 
+  constructor(private exchangeRateService: ExchangeRateService) {}
+
+  ngOnInit(): void {
+    this.exchangeRateService.getExchangeRates().subscribe((data) => {
+      this.exchangeRates = Object.entries(data.conversion_rates).map(
+        ([currency, value]) => ({
+          currency,
+          value,
+        })
+      );
+    });
+  }
 }
