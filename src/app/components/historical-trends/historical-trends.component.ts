9eeed6aa57b4a48da043bd223e12cb824f5f6ec1
Hazim <hazim.shaifulbahari@cimb.com>
2025-05-05 10:55:15 +0800

    block current date and future date because API disallows

--- src/app/components/historical-trends/historical-trends.component.ts
+++ src/app/components/historical-trends/historical-trends.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ExchangeRateService } from 'src/app/services/exchange-rate.service';
-import {
-  Chart,
-  registerables
-} from 'chart.js';
+import { Chart, registerables } from 'chart.js';
 
 Chart.register(...registerables);
 
@@ -12,16 +9,21 @@ Chart.register(...registerables);
   templateUrl: './historical-trends.component.html',
   styleUrls: ['./historical-trends.component.scss'],
 })
-export class HistoricalTrendsComponent implements OnDestroy {
+export class HistoricalTrendsComponent implements OnInit, OnDestroy {
   baseCurrency = 'USD';
   targetCurrenciesStr = 'MYR,SGD,AUD';
-  selectedDate: string; // Use a date instead of interval
+  selectedDate!: string; // Use a date instead of interval
   chart: any;
 
-  constructor(private exchangeRateService: ExchangeRateService) {
-    // Default to today's date
-    const today = new Date();
-    this.selectedDate = today.toISOString().split('T')[0]; // Format: YYYY-MM-DD
+  constructor(private exchangeRateService: ExchangeRateService) {}
+
+  ngOnInit(): void {
+    // Set default to yesterday's date
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    // Format: YYYY-MM-DD
+    this.selectedDate = yesterday.toISOString().split('T')[0]; 
+    this.loadHistoricalData()
   }
 
   ngOnDestroy(): void {
@@ -29,6 +31,12 @@ export class HistoricalTrendsComponent implements OnDestroy {
       this.chart.destroy();
     }
   }
+
+  disableFutureDates = (date: Date | null): boolean => {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); // Reset time to midnight
+    return date ? date < today : false;
+  };
 
   loadHistoricalData(): void {
     const targets = this.targetCurrenciesStr.split(',').map((c) => c.trim()).slice(0, 3);
@@ -47,12 +55,12 @@ export class HistoricalTrendsComponent implements OnDestroy {
     const labels = data.map((d: any) => d.currency);
     const datasets = [
       {
-      label: `Exchange Rate of ${this.baseCurrency} on ${this.selectedDate}`,
-      data: data.map((entry: any) => entry.rate),
-      borderColor: this.getRandomColor(),
-      backgroundColor: this.getRandomColor(),
-      fill: false,
-      tension: 0.3,
+        label: `Exchange Rate of ${this.baseCurrency} on ${this.selectedDate}`,
+        data: data.map((entry: any) => entry.rate),
+        borderColor: this.getRandomColor(),
+        backgroundColor: this.getRandomColor(),
+        fill: false,
+        tension: 0.3,
       },
     ];
   
